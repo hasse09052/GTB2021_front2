eb47803a844d0644d081b9979e8b27ffd5175c81
changmaru <doichan.kb@gmail.com>
2021-05-27 17:34:59 +0900

    LP

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -6,10 +6,22 @@
     <div class="bg">
       <figure class="logoWrap"><img src="../assets/img_logo.png" alt=""></figure>
       <h1 class="catchphrase">朝の準備を<br>かんたんに、正確に</h1>
-      <div class="container">
-        <div class="titleWrap">
-          <h1 class="title">使いかた</h1>
+
+      <img class="separator" src="../assets/img_separator.png" alt="">
+      <div class="features">
+        <div class="feature-description">
+          <img src="../assets/img_clock.png" alt="">
+          <h2>毎日あなた専用の持ちものリスト</h2>
+          <p>持ちものを毎朝カスタマイズして通知。<br>あなたが絶対に持っていくものはなんですか？</p>
         </div>
+        <div class="feature-description">
+          <img src="../assets/img_rain.png" alt="">
+          <h2>雨の日に忘れがちな折りたたみ傘</h2>
+          <p>天気に応じて必要になる傘や日焼け止め。<br>その日の天気予報から、<br>Prepasyが自動で持ちものリストに追加します。</p>
+        </div>
+      </div>
+      <img class="separator" src="../assets/img_separator.png" alt="">
+      <div class="container">
         <ol class="imageList">
           <li class="imageList__imgWrap"><img src="../assets/img_step1.png" alt="忘れがちな持ち物を登録"></li>
           <li class="imageList__imgWrap"><img src="../assets/img_step2.png" alt="寝ているあいだに、明日必要なものをリストアップ"></li>
@@ -105,30 +117,77 @@ export default {
   }
 }
 
+.separator {
+  display: block;
+  margin: 80px auto;
+  padding: 0;
+  height: 60px;
+  @media only screen and (max-width: 767px) {
+    height: 32px;
+    margin: 40px auto;
+  }
+}
+
+.features {
+  display: flex;
+  justify-content: space-between;
+  width: 1200px;
+  margin: 0 auto; 
+  @media only screen and (max-width: 767px) {
+    width: 320px;
+    display: block;
+  }
+  
+
+  .feature-description {
+    width: 520px;
+    @media only screen and (max-width: 767px) {
+      width: 320px;
+      margin: 0 auto;
+    }
+    img {
+      width: 100%;
+    }
+    h2 {
+      margin-top: 32px;
+      margin-bottom: 24px;
+      font-size: 20px; 
+    }
+    p {
+      margin: 0;
+    }
+
+    &:nth-child(1) {
+     margin-bottom: 40px;
+    }
+  }
+}
+
 .imageList {
   display: flex;
   justify-content: space-between;
-  padding: 0;
+  padding: 0 0 80px 0;
   margin: 0 auto;
   list-style: none;
-
   @media only screen and (max-width: 767px) {
     display: block;
+    margin: 0 auto;
   }
 
   &__imgWrap {
     flex: 0 1 378px;
     border-radius: 20px;
     box-shadow: 0 0 4px rgba($color: #333338, $alpha: 0.1);
+    @media only screen and (max-width: 767px) {
+      border-radius: 18px;
+      margin: 0 40px;
+    }
+
     &:nth-child(2) {
       margin: 0 33px;
       @media only screen and (max-width: 767px) {
-        margin: 0 0 40px;
+        margin: 40px;
       }
-    }
-    @media only screen and (max-width: 767px) {
-      border-radius: 18px;
-      margin: 0 0 40px;
     }
   }
 }
